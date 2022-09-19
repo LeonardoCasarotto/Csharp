cfb0b8f176529cb0fd45b8156ace29da51d15598
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2022-09-19 18:17:37 +0200

    lupi pecore

---
+++ Es 1909/CasarottoLupiPecore/CasarottoLupiPecore/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CasarottoLupiPecore
+{
+
+    public partial class Form1 : Form
+    {
+        public static char[] lettere;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            string file = System.IO.File.ReadAllText(@"../../Pecore_Cavoli.csv");
+            file =file.Replace(";", "");
+            lettere = file.ToCharArray();
+
+            lettere =MyFunc.Ordina(lettere);
+
+            MessageBox.Show(MyFunc.Ordina(lettere).ToString());
+           
+        }
+    }
+}
---
+++ Es 1909/CasarottoLupiPecore/CasarottoLupiPecore/MyFunc.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CasarottoLupiPecore
+{
+    static class MyFunc
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+        
+        
+        [STAThread]
+
+        
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+           
+
+        }
+
+        public static char[] Ordina(char[] lettere)
+        {
+           
+                int lg = lettere.Length;
+
+                if (lg == 1)
+                    return lettere;
+
+                for (int i = 0; i < lg - 1; i++)
+                {
+                    switch (lettere[i])
+                    {
+
+                        case 'c':
+                            if (lettere[i] > lettere[i + 1])
+                            {
+
+                                char temp = lettere[i + 1];
+                                lettere[i + 1] = lettere[i];
+                                lettere[i] = temp;
+
+                            }
+
+                            break;
+
+                        case 'p':
+                            if (lettere[i + 1] == 'c')
+                            {
+                                char temp = lettere[i + 1];
+                                lettere[i + 1] = lettere[i];
+                                lettere[i] = temp;
+                            }
+                            break;
+
+                        case 'l':
+                            if (lettere[i + 1] != 'l')
+                            {
+                                char temp = lettere[i + 1];
+                                lettere[i + 1] = lettere[i];
+                                lettere[i] = temp;
+                            }
+                            break;
+
+                    }
+                }
+                
+
+                return Ordina(lettere);
+            
+
+
+        }
+
+    }
+}
