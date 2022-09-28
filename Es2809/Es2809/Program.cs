3a28cc24c734978901c4a2a0a030b0603a5ea315
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2022-09-28 13:14:41 +0200

    recursion exercise

---
+++ Es2809/Es2809/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+namespace Es2809
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 7, 4, 3, 10, 3 };
+
+            WriteLine(myfunc(arr, 0, 5, 3));
+
+
+        }
+
+        static int myfunc(int[] arr, int start, int end, int x)
+        {
+            if (start == end+1)
+            {
+                return 0 ;
+            }
+            if (arr[start] == x)
+            {
+                return 1 + (myfunc(arr, start + 1, end, x));
+            }
+
+            else
+            {
+                return (myfunc(arr, start + 1, end, x));
+            }
+        }
+        
+    }
+}
