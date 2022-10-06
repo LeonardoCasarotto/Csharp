8d35fc5398f352dcb580214c82372cef50e773db
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2022-10-06 15:33:12 +0200

    d

---
+++ Es0510/Es0510/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Es0510
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(matrice(3, 3));
+        }
+        
+        static int matrice(int r, int c)
+        {
+            //caso base
+            if (r == 1 || c == 1) 
+                return 1;
+
+
+            else
+                return (matrice(r - 1, c) + matrice(r, c - 1));
+        }
+    }
+}
