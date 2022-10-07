10feafe22bcec5133ce05a02faf7be4aa201e076
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2022-10-07 10:53:26 +0200

    hanoi

---
+++ Es0710/Es0710/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace Es0710
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hanoi(4, 'A', 'B', 'C');
+        }
+        static void Hanoi(int n, char from, char to, char current )
+        {
+            //caso base
+            if (n == 1)
+            {
+                WriteLine("Disco 1 spostato da " + from + " a " + to);
+                return;
+            }
+            Hanoi(n - 1, from, current,to);
+            WriteLine("Disco " + n + " spostato da " + from + " a " + to);
+            Hanoi(n - 1, current, to, from);
+        }
+    }
+}
