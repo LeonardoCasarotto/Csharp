b25b25eb14e53063a0c0e9d6b4cb9925e06f1171
CASAROTTO LEONARDO <8293359@itisrossi.edu.it>
2022-10-24 11:38:20 +0200

    initial commit

---
+++ Es1024/Es1024/Auto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Es1024
+{
+    public class Auto:Veicolo
+    {
+        private int Doors;
+        private string Power;
+
+
+        public Auto(int id, string brand, int year, int cyl, int doors,string power) : base(id, brand, year, cyl) {
+            Doors = doors;
+            Power = power;
+        }
+
+
+    }
+}
