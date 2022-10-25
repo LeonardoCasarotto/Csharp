3cfcbfa444b5997f37527d7de64e836245ebc9bb
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2022-10-25 22:05:43 +0200

    update

--- Es1024/Es1024/Garage.cs
+++ Es1024/Es1024/Garage.cs
@@ -7,10 +7,23 @@ namespace Es1024
     public class Garage
     {
         private Veicolo[] gar;
+
         public Garage()
         {
             gar = new Veicolo[15];
         }
 
+        public string GetSituazione()
+        {
+            string m = "";
+            for(int i = 0; i < gar.Length; i++)
+            {
+                m += i + 1;
+                if (gar[i] == null) m += "LIBERO";
+                else m += "OCCUPATO";
+            } 
+            return m;
+        }
+        
     }
 }
--- Es1024/Es1024/Program.cs
+++ Es1024/Es1024/Program.cs
@@ -45,21 +45,83 @@ namespace Es1024
             switch (ReadLine())
             {
                 case "A":
-                    int anno, cilindrata;
-                    string marca;
+                    int anno, cilindrata,doors;
+                    string marca,alim;
                     WriteLine("Inserire l'anno di fabbricazione del veicolo:");
                     bool c = true;
                     while (c)
                     {
                         c = Int32.TryParse(ReadLine(), out anno);
-                        if (!c || anno < 1950 || anno < DateTime.Now.Year)
+                        if (!c || anno < 1950 || anno > DateTime.Now.Year)
+                        {
+                            Errore();
+                        }
+                        else c = false;
+                    }
+                    c = true;
+                    WriteLine("Inserire la marca del veicolo");
+                    while (c)
+                    {
+                        marca = ReadLine();
+                        if (marca.Length < 1)
+                        {
+                            Errore();
+                        }
+                        else c = false;
+                    }
+                    c = true;
+                    WriteLine("Inserire la cilindrata del veicolo");
+                  
+                    while (c)
+                    {
+                        c = Int32.TryParse(ReadLine(), out cilindrata);
+                        if (!c || cilindrata < 10|| cilindrata>5000)
+                        {
+                            Errore();
+                        }
+                        else c = false;
+                    }
+
+                    WriteLine("Inserire il numero di porte dell'auto");
+                    while (c)
+                    {
+                        c = Int32.TryParse(ReadLine(), out doors);
+                        if (!c || doors < 1|| doors > 7)
                         {
                             Errore();
                         }
                         c = false;
                     }
-                    c = false;
-                    
+
+                    WriteLine("infine selezionare 1 se l'automobile si alimenta a gasolio, 2 per la benzina e 3 elettrica");
+
+                    c = true;
+                    while (c)
+                    {
+                        switch (ReadLine())
+                        {
+                            case "1":
+                                alim = "Gasolio";
+                                c = false;
+                                break;
+                            case "2":
+                                alim = "Benzina";
+                                c = false;
+                                break;
+                            case "3":
+                                alim = "elettrica";
+                                c=false;
+                                break;
+                            default:
+                                Errore();
+                                break;
+                        }
+                    }
+                    WriteLine("Segliere infine il numero del posteggio che si desidera occupare\n"//todo add arr garade)
+                        );
+
+
+
 
                     break;
                case "F":
