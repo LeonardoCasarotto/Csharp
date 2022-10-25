9f93b97bedefacb8ed877666dab812a85e49ef79
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2022-10-25 07:16:10 +0200

    upd

---
+++ Es1024/Es1024/Garage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Es1024
+{
+    public class Garage
+    {
+        private Veicolo[] gar;
+        public Garage()
+        {
+            gar = new Veicolo[15];
+        }
+
+    }
+}
--- Es1024/Es1024/Program.cs
+++ Es1024/Es1024/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using static System.Console;
 
 namespace Es1024
 {
@@ -6,7 +6,64 @@ namespace Es1024
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool m = true;
+
+            while (m)
+            {
+                Menu();
+                switch (ReadLine())
+                {
+                    case "1":
+
+                        break;
+
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    default:
+                        Errore();
+                        break;
+                }
+
+            }
         }
+
+        static void Menu()
+        {
+            WriteLine("Segliere cosa si desidera fare:" +
+                "\n1.Inserimento Veicolo" +
+                "\n2.Estrazione veicolo" +
+                "\n3.Stampa situazione corrente");
+        }
+
+        static void New()
+        {
+            WriteLine("Inserire A per parcheggiare un'automobile, F per un furgone e M per una motocicletta");
+            switch (ReadLine())
+            {
+                case "A":
+                    int anno, cilindrata;
+                    string marca;
+                    WriteLine("Inserire l'anno di fabbricazione del veicolo:");
+                    //undone insert park verification.
+                    break;
+                case "F":
+                    break;
+                case "M":
+                    break;
+                default:
+                    Errore();
+                    break;
+            }
+        }
+        static void Errore()
+        {
+            WriteLine("Errore! Ritentare.");
+        }
+        // TODO: ADD ERROR HANDLING 
+        // TODO: COMPLETE
+
+
     }
 }
