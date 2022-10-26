fefc0c372f120b0c4b7f6c69e652a44f04a05744
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2022-10-26 09:52:05 +0200

    update

--- Es1024/Es1024/Veicolo.cs
+++ Es1024/Es1024/Veicolo.cs
@@ -24,6 +24,7 @@ namespace Es1024
             
             
         }
+       
 
         public int GetId()
         {
@@ -42,5 +43,7 @@ namespace Es1024
         {
             return Cyl;
         }
+        //TODO GET ID and keep count of it.
+        
     }
 }
