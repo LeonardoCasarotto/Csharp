8de05d4c4b4102783c27821be13fc369deb44bbc
CASAROTTO LEONARDO <8293359@itisrossi.edu.it>
2022-11-11 10:28:20 +0100

    prova

---
+++ Es1111/Es1111/Animale.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Es1111
+{
+    public abstract class Animale
+    {
+        private string nome;
+        private string verso;
+        private string habitat;
+
+
+        public abstract string Verso();
+
+        public abstract string Vive();
+
+        public abstract bool Mangia();
+    }
+}
