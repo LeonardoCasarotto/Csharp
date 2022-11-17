96d3ed368efd3118e8ca64b499fda0f17c40083b
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2022-11-17 21:28:11 +0100

    Es1117

---
+++ Es1117/Es1117/Intefaces/IVola.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Es1117
+{
+    interface IVola
+    {
+        bool Decolla();
+        bool Atterra();
+    }
+}
---
+++ Es1117/Es1117/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Es1117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //todo write program
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
