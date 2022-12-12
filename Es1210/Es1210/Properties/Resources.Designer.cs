cd2d62c86ce77731f7643c640183d027c730f19f
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2022-12-12 20:03:49 +0100

    update delegates exercises

--- Es1210/Es1210/Properties/Resources.Designer.cs
+++ Es1210/Es1210/Properties/Resources.Designer.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 
-namespace Es1212.Properties
+namespace Es1210.Properties
 {
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
@@ -43,7 +43,7 @@ namespace Es1212.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Es1212.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Es1210.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
