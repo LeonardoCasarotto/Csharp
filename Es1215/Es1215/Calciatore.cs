b64c93562c7bfe4fef177eb90078e6cfe9c70c5d
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2022-12-16 21:17:04 +0100

    update

--- Es1215/Es1215/Calciatore.cs
+++ Es1215/Es1215/Calciatore.cs
@@ -9,19 +9,92 @@ namespace Es1215
         string nome;
         string cognome;
         int età;
-        bool personlizzazione;
+        bool personalizzazione;
 
-        public Atleta(string n, string c, int e,bool p)
+        public Atleta(string n, string c, int e, bool p)
         {
             this.nome = n;
             this.cognome = c;
             this.età = e;
-            this.personlizzazione = p;
+            this.personalizzazione = p;
         }
+        public Atleta()
+        {
+        }
+
+        public string GetNome()
+        {
+            return this.nome;
+        }
+        public void SetNome(string name)
+        {
+            this.nome = name;
+        }
+        public string GetCognome()
+        {
+            return this.cognome;
+        }
+        public void SetCognome(string cg)
+        {
+            this.cognome = cg;
+        }
+        public bool GetPers()
+        {
+            return this.personalizzazione;
+        }
+        public string personalizza()
+        {
+            return this.nome + "\n" + this.cognome + "\n";
+        }
+
+
+
+
+
         // classe publisher: squadra
         /*public event eventhandler PersonalizzaMaglia
          * dentro atleta va un metodo con firma event handler,se si desidera si aggancia il metodo 
         // classe che aderisce:*/
     }
 
+    public class Squadra
+    {
+        string nome;
+        Atleta[] giocatori;
+        int index;
+
+
+        public Squadra(string name, int part)
+        {
+            this.nome = name;
+            giocatori = new Atleta[part];
+            index = 0;
+        }
+        public void AddGiocatore(string n, string c, int e, bool p)
+        {
+
+            giocatori[index] = new Atleta(n, c, e, p);
+            index++;
+        }
+        public Atleta GetGiocatore(int n)
+        {
+
+            return giocatori[n];
+        }
+        public int GetNpart()
+        {
+            return index;
+        }
+        public string PersonalizzaMaglia(int n)
+        {
+            return giocatori[n].personalizza() + n;
+        }
+
+    }
+
+
+
+
+
 }
+
