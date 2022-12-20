83264d2d410200989593a280fd7beb80ac98c1ce
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2022-12-20 08:47:33 +0100

    es1220

---
+++ Es2012/Es2012/EventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Es1220
+{
+    public class EventArgs
+    {
+        string name;
+
+        private void SetName(string name)
+        {
+            this.name = name;
+        }
+        public string GetName()
+        {
+            return this.name;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Es2012/Es2012/NameChangeEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Es1220
+{
+    public class NameChangeEventArgs:EventArgs
+    {
+
+    }
+}
