3b72dde8742f1998879baf75adb5f909666691c2
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2023-01-17 08:44:36 +0100

    update
    threads exercises

---
+++ Es0117/Es0117/Classes/Account.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Es0117.Classes
+{
+    public class Account
+    {
+        private readonly object BalanceLock = new object();
+        private decimal balance;
+
+
+        public Account(decimal inizio) => balance = inizio;
+
+
+
+        public decimal prelievo(decimal soldi)
+        {
+            
+            if (soldi <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Non è possibile prelevare valori nulli o negativi");
+            }
+            lock (BalanceLock)
+            {
+                if (balance < soldi)
+                {
+                    throw new ArgumentOutOfRangeException("Non è possibile prelevare valori superiori a quanto in deposito");
+                }
+                balance -= soldi;
+                return soldi;
+            }
+        }
+
+        public decimal deposito(decimal cash)
+        {
+            if (cash <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Attenzione, non è possibile depositare valori nulli o negativi.");
+            }
+            lock (BalanceLock)
+            {
+                balance += cash;
+            }
+            return balance;
+        }
+
+        public decimal GetBalance()
+        {
+            lock (BalanceLock)
+            {
+                return this.balance;
+
+            }
+        }
+
+
+    }
+}
---
+++ Es0117/Es0117/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace Es0117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Benvenuti, per iniziare inserire il valore iniziale del deposito bancario");
+            bool test = false;
+            decimal init = -1;
+            while (!test)
+            {
+                test= Decimal.TryParse(ReadLine(), out init);
+                if (!test || init < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Attenzione! Inserire un valore superiore o uguale a zero.");
+                }
+                //TODO implement threads
+                //TODO implement program
+            }
+        }
+
+
+       
+    }
+}
