c4e3f4570c75f66291a375b72fb2453bd6917277
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2023-01-18 09:45:13 +0100

    Finished exercises

---
+++ Es0118B/Es0118B/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Es0118B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch clock = new Stopwatch();
+
+            //Thread tmain = new Thread();
+            clock.Start();
+            for(int i = 0; i < 100; i++)
+            {
+                Write("A");
+                Thread.Sleep(10);
+            }
+            Write("\n");
+            for(int i = 0; i < 100; i++)
+            {
+                Write("B");
+                Thread.Sleep(10);
+            }
+            clock.Stop();
+
+            WriteLine("\nTempo trascorso uguale a " + clock.ElapsedMilliseconds+ "ms");
+        }
+    }
+}
---
+++ Es0118C/Es0118C/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+
+namespace Es0118C
+{
+    class Program
+    {
+        static int A, B, C, D, E, F,G,H;
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(Proc1);
+            Thread t2 = new Thread(Proc2);
+            t1.Start();
+            t2.Start();
+
+            A = 10;
+            D = A + 5;
+            Console.WriteLine("D="+D);
+            t1.Join();
+            G = E - D;
+            Console.WriteLine("G="+G);
+            t2.Join();
+            H = F + G;
+            Console.WriteLine("G="+G);
+        }
+        static void Proc1()
+        {
+            B = 20;
+            E = B * 2;
+            Console.WriteLine("E="+E);
+
+        }
+        static void Proc2()
+        {
+            C = 30;
+            F = C * C;
+            Console.WriteLine("F="+F);
+        }
+    }
+}
