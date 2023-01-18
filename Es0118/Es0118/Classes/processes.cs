2fc2b0360f4dc6b3d1e59a73c989837368882568
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2023-01-18 09:07:39 +0100

    Es0118_A

---
+++ Es0118/Es0118/Classes/processes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using static System.Console;
+
+namespace Es0118.Classes
+{
+    public class processes
+    {
+        public static void ThreadA()
+        {
+            WriteLine("Thread_A Start\n");
+            for(int j = 0; j < 100; j++)
+            {
+                Write("A");
+            }
+            ReadKey();
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Es0118/Es0118/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+using Es0118.Classes;
+namespace Es0118
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread tA = new Thread(processes.ThreadA);
+            tA.Start();
+
+            WriteLine("Thread_B Start\n");
+            for(int j = 0; j < 100; j++)
+            {
+                Write("B");
+            }
+            tA.Join();
+
+            
+
+        }
+    }
+}
