e554e44f416ef06582ad4c0e31d534c1d802917d
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2023-01-19 13:15:47 +0100

    update

---
+++ Es0119/Es0119/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+
+namespace Es0119
+{
+    enum Tipo{
+        add,sub
+
+    }
+    class Program
+    {
+        static int num1 = 200;
+        static int num2 = 300;
+        
+        static void Main(string[] args)
+        {
+            int qta;
+            Console.WriteLine("Inserire quantità");
+            int.TryParse(Console.ReadLine(), out qta);
+
+            Shift s1 = new Shift(qta,Tipo.add);
+            Shift s2 = new Shift(qta, Tipo.sub);
+
+
+            Thread t1 = new Thread(Operazione);
+            t1.Start(s1);
+            Thread t2 = new Thread(Operazione);
+            t2.Start(s2);
+
+            t1.Join();
+            t2.Join();
+            Console.WriteLine("Numero 1 uguale a "+num1+"\nNumero 2 uguale a "+num2);
+
+        }
+
+        static void Operazione(object o)
+        {
+            Shift s = o as Shift;
+            switch (s.operazione)
+            {
+                case Tipo.add:
+                    num1 += s.qta;
+                    break;
+                case Tipo.sub:
+                    num2 -= s.qta;
+                    break;
+            }
+        }
+    }
+    
+    //object shift for passing the quantity as a parameter
+    class Shift
+    {
+        public int qta { get; set; }
+        public Tipo operazione;
+
+        public Shift(int q, Tipo operazione)
+        {
+            this.qta = q;
+            this.operazione = operazione;
+        }
+    }
+}
