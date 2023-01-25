9142a38cc79191820519f23c170412e611a22c88
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2023-01-25 12:57:24 +0100

    update

---
+++ Es0123/Es0123/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Es0123
+{
+    class Program
+    {
+
+
+        static int rows, cols;
+        static int[,] matrix,matrice;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserire il numero di righe:");
+            rows= int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserire il numero di colonne:");
+            cols = int.Parse(Console.ReadLine());
+
+            matrix = new int[rows, cols];
+
+
+
+
+        }
+    }
+}
