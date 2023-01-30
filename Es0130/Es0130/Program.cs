1a66c740e7c54ead8cc8123d15605deff78b3dde
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2023-01-30 11:39:02 +0100

    new es

---
+++ Es0130/Es0130/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Es0130
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
