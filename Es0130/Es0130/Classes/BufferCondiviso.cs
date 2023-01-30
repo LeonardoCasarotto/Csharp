d6bb7dda771fab08ca609910f70d7f30a30479f7
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2023-01-30 15:24:22 +0100

    finished es 0130

--- Es0130/Es0130/Classes/BufferCondiviso.cs
+++ Es0130/Es0130/Classes/BufferCondiviso.cs
@@ -1,10 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Es0130.Classes
 {
-    public class BufferCondiviso
+
+    namespace CThreadWaitPulse_base
     {
+        public class BufferCondiviso
+        {
+            private char buffer = ' ';                  // buffer shared by producer and consumer threads  
+            private int occupiedBufferCount = 0;
+            // occupiedBufferCount si ricorda se                                      
+            // (1) il buffer contiene un valore generato dal produttore e non ancora consumato,
+            // NON si può scriverci sopra, si può consumare
+            // (0) il valore del buffer è stato consumato,
+            // si può scriverci sopra, NON si può consumare il suo valore
+
+
+            static readonly object _locker = new object();
+            public char Buffer
+            {
+                get  //lettore consumatore
+                {
+                    char bufferCopy;
+                    lock (_locker)  //sezione critica
+                                    //entra il thread che trova l'oggetto _locker libero
+                    {
+                        // se non ci sono dati da leggere
+                        if (occupiedBufferCount == 0)
+                        {   //mette il thread invocante nello stato WaitSleepJoin
+                            Console.WriteLine(Thread.CurrentThread.Name + "prova a leggere");
+                            DisplayState("Buffer vuoto. " + Thread.CurrentThread.Name + " wait");
+                            Monitor.Wait(_locker); //il thread va in waiting,
+                                                   //rilascia _locker
+                                                   //attende di essere risvegliato
+                                                   //e di rientrare in possesso del _locker
+                        }
+                        --occupiedBufferCount;
+                        DisplayState(Thread.CurrentThread.Name + "legge " + buffer);
+                        bufferCopy = buffer;  //salvo su variabile locale al get
+                        Monitor.Pulse(_locker);  //risveglia un thread dalla coda dei waiting
+                                                 //notifica il rilascio del _locker
+
+                    }
+                    return bufferCopy;
+                }
+                set  //scrittore-produttore
+                {
+                    lock (_locker)   //sezione critica
+                                     //entra il thread che trova l'oggetto _locker libero                     
+                    {   //se non ci sono posizioni libere
+                        if (occupiedBufferCount == 1)
+                        {
+                            //mette il thread di chiamata nello stato WaitSleepJoin
+                            Console.WriteLine(Thread.CurrentThread.Name + "prova a scrivere");
+                            DisplayState("Buffer pieno. " + Thread.CurrentThread.Name + "wait.");
+                            Monitor.Wait(_locker);//il thread va in waiting,
+                                                  //rilascia _locker
+                                                  //attende di essere risvegliato
+                                                  //e di rientrare in possesso del _locker
+                        }
+
+                        buffer = value;
+                        ++occupiedBufferCount;
+                        DisplayState(Thread.CurrentThread.Name + "scrive " + buffer);
+                        Monitor.Pulse(_locker); //risveglia un thread dalla coda dei waiting
+                                                //notifica il rilascio del _locker
+                    }
+                }
+                //bufferCopy = buffer;  //salvo su variabile locale al get
+            }
+
+            // visualizza loperazione corrente e lostato del buffer 
+            public void DisplayState(string operation)
+            {
+                //Un numero intero con segno che indica
+                //la lunghezza totale del campo
+                //in cui è inserito l'argomento e se lo è
+                //allineato a destra (un numero intero positivo) o allineato a sinistra
+                //(un numero intero negativo)
+                Console.WriteLine($"{operation,-35}{buffer}{occupiedBufferCount}");
+            }
+        } //end BufferCondiviso
     }
 }
--- Es0130/Es0130/Classes/Consumatore.cs
+++ Es0130/Es0130/Classes/Consumatore.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using Es0130.Classes.CThreadWaitPulse_base;
+using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Text;
 
 namespace Es0130.Classes
 {
     public class Consumatore
     {
+
+        static BufferCondiviso[] arr;
+        static Random wait;
+        static string parola;
+        int l;
+
+        public Consumatore( BufferCondiviso[] array, Random w, int lg)
+        {     
+            arr = array;
+            wait = w;
+            parola = "";
+            l = lg;
+        }
+
+        public void Consume()
+        {
+            for (int i = 0; i < l; i++)
+            {
+                Thread.Sleep(wait.Next(0, 2000));
+                parola += arr[i % arr.Length].Buffer;
+
+            }
+            Console.WriteLine("\n\nLa parola corrisponde a  " + parola);
+        }
+
+
+
+
+
+
     }
 }
