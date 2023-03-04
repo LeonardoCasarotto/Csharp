a6e6fc908d4e47f9f1440f32642d42269a4b28b9
leonardo <leonardo.casar8@gmail.com>
2023-03-04 14:28:39 +0100

    update

--- Es0302/Es0302/Program.cs
+++ Es0302/Es0302/Program.cs
@@ -4,15 +4,17 @@ namespace Es0302
 {
     class Program
     {
-        static Pila<char> pil = new Pila<char>();
+        static Pila<int> pil = new Pila<int>();
         static void Main(string[] args)
         {
-            string exp = "12*45-2*";
 
+            string exp = "12*45-2*";
+            Console.WriteLine("Espressione di partenza :\n"+exp);
             sel(exp);
 
             for(int i = 0; i < pil.aTop ; i++)
             {
+                Console.WriteLine("Il risultato uguale a :");
                 Console.WriteLine(pil.Pop());
             }
             
@@ -29,9 +31,65 @@ namespace Es0302
         {
             int posizia = 0;
 
+
             for(int i = 0; i < c.Length; i++)
             {
-                switch(c[i];:
+
+                switch (c[i])
+                {
+                    default:
+                        pil.Push(Convert.ToInt32(c[i]-'0'));
+                        
+                        break;
+                    case '+':
+                        int sum=pil.Pop();
+                        for(int j = 0; j < pil.aTop; j++)
+                        {
+                            
+                            sum += pil.Pop();
+                            
+                        }
+                        
+                        pil.Push(sum);
+
+                        break;
+                    case '-':
+                        int diff = pil.Pop();
+                        for (int j = 0; j < pil.aTop; j++)
+                        {
+                           
+                            diff -= pil.Pop();
+                        }
+                        
+                        pil.Push(diff);
+                        break;
+                    case '*':
+                        int molt = pil.Pop();
+
+                        for (int j = 0; j < pil.aTop; j++)
+                        {
+
+                            molt *= pil.Pop();
+                            
+                        }
+
+                        pil.Push(molt);
+                        break;
+                    case '/':
+                        int div = pil.Pop();
+                        for (int j = 0; j < pil.aTop; j++)
+                        {
+                            
+                            div /= pil.Pop();
+                        }
+
+                        pil.Push(div);
+                        break;
+
+
+
+                }
+                
 
             }
         }
