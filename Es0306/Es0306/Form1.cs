33ec757fb9ac253042a94f9917c278b2ebcf15ae
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2023-03-07 11:25:08 +0100

    es0306

--- Es0306/Es0306/Form1.cs
+++ Es0306/Es0306/Form1.cs
@@ -15,7 +15,54 @@ namespace Es0306
         public Form1()
         {
             InitializeComponent();
-        }
+    
+            TableLayoutPanel scacchiera;
+            
+                InitializeComponent();
+                scacchiera = new TableLayoutPanel();
+
+                scacchiera.ColumnCount = 8;
+                scacchiera.RowCount = 8;
+                scacchiera.Dock = DockStyle.Fill;
+
+                scacchiera.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+                scacchiera.ColumnStyles.Clear();
+                scacchiera.RowStyles.Clear();
+                //create a chess board with alternating colors and all squares being same size and filling the form
+
+                for (int i = 0; i < 8; i++)
+                {
+                    scacchiera.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 12.5F));
+                    scacchiera.RowStyles.Add(new RowStyle(SizeType.Percent, 12.5F));
+                }
+
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        Label l = new Label();
+                        l.Dock = DockStyle.Fill;
+                        l.TextAlign = ContentAlignment.MiddleCenter;
+                        l.BackColor = (i + j) % 2 == 0 ? Color.White : Color.Black;
+                        scacchiera.Controls.Add(l, i, j);
+                    }
+                }
+                //make the window a square
+                this.Width = this.Height;
+
+                //make the window not resizable and appear in the center of the screen
+                this.FormBorderStyle = FormBorderStyle.FixedSingle;
+                this.StartPosition = FormStartPosition.CenterScreen;
+
+                this.Controls.Add(scacchiera);
+
+
+
+
+
+            }
+        
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
