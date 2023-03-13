3d4f7b4a5c49a97fc14eeac90dae2755d1188a72
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2023-03-13 12:10:18 +0100

    es0305

--- Es0305/Es0305/Program.cs
+++ Es0305/Es0305/Program.cs
@@ -10,31 +10,55 @@ namespace Es0305
         {
             int same = 0;
             int[,] matrice = { { 0, 1, 0 }, { 0, 0, 0 }, { 0, 1, 0 } };
-            Stack<int> pila = new Stack<int>();
-            for(int i = 0; i < 3; i++)
+            Stack<Vip> pila = new Stack<Vip>();
+            for(int i = 0; i < matrice.GetLength(1); i++)//colonna
             {
-                for(int j = 0; j < 3; j++)
+                int con = 0;
+                bool vip = true;
+                for (int j = 0; j < matrice.GetLength(0); j++)//riga
                 {
-                    if (matrice[i, j] == 1&&i!=j)
+                    //verifico se in ciascuna riga della colonna se un valore è uguale a 1
+                    
+                    if (matrice[j,i] ==1)
                     {
-                        bool vip = true;
-
-                        for(int m = 0; m < 3; m++)
+                        con++;
+                        //ho trovato una persona ne conosce un'altra
+                        
+                        for(int m = 0;m < matrice.GetLength(0);m++) 
                         {
-                            if (matrice[j, m] == 1)
+                            
+                            if (matrice[i, m] == 1)
                             {
+                                
                                 vip = false;
                                 break;
                             }
-                            
+
+
                         }
-                        if (vip) pila.Push(j);
+                        
+
                     }
+
+                }
+                
+                if (vip)
+                {
+
+
+                    pila.Push(new Vip(i, con));
 
                 }
             }
             if (pila.Count == 0) Console.WriteLine(-1);
-            else Console.WriteLine(1);
+            else
+            {
+                Console.WriteLine(pila.Count);
+                foreach(Vip v in pila)
+                {
+                    if(v.followers>0)Console.WriteLine("Il vip a riga "+v.riga+" è conosciuto da "+v.followers+" persone");
+                }
+            }
 
            
 
---
+++ Es0305/Es0305/Vip.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Es0305
+{
+    class Vip
+    {
+        public int riga { get; set; }
+        public int followers { get; set; }
+        public Vip(int r, int f)
+        {
+            this.riga = r;
+            this.followers = f;
+        }
+    }
+}
