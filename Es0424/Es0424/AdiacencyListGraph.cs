760ee143e4adb235b1d9e3f6190c00718cb6c3ce
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2023-04-25 23:00:26 +0200

    update es 0404

---
+++ Es0424/Es0424/AdiacencyListGraph.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Es0424
+{
+    class AdiacencyListGraph
+    {
+        private int noVertices;
+        private LinkedList<int>[] arrLinkedList; //array di linked list,
+        //come da definizione, ogni nodo possiede una lista di nodi al quale può essere
+        //collegato
+
+        public AdiacencyListGraph(int n)
+        {
+            noVertices = n;
+            arrLinkedList = new LinkedList<int>[n];
+
+
+            //initialization of the lists
+            for (int i = 0; i < noVertices; i++)
+            {
+                arrLinkedList[i] = new LinkedList<int>();
+            }
+        }
+
+        public void addEdge(int v, int ad)
+        {
+            arrLinkedList[v].AddLast(ad);
+            //
+        }
+
+
+
+    }
+}
---
+++ Es0424/Es0424/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Es0424
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
