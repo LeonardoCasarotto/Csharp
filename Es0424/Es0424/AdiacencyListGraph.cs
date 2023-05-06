8a8ec200b656c18a31c5d61a7dbd277ae58b8b11
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2023-05-06 08:40:30 +0200

    update

--- Es0424/Es0424/AdiacencyListGraph.cs
+++ Es0424/Es0424/AdiacencyListGraph.cs
@@ -6,8 +6,9 @@ namespace Es0424
 {
     class AdiacencyListGraph
     {
-        private int noVertices;
-        private LinkedList<int>[] arrLinkedList; //array di linked list,
+        public int noVertices { get; }
+        private LinkedList<int>[] arrLinkedList;
+        //array di linked list,
         //come da definizione, ogni nodo possiede una lista di nodi al quale può essere
         //collegato
 
@@ -31,6 +32,57 @@ namespace Es0424
         }
 
 
+        public string PrintAdjencyList()
+        {
+            string topr = "";
+            for (int i = 0; i < arrLinkedList.Length; i++)
+            {
+                topr += " [Node val: " + i + "with neighbors ";
+                foreach (var n in arrLinkedList[i])
+                {
+                    topr += " --> " + n;
+                }
+                topr += "]";
+            }
+
+            return topr;
+        }
+
+        public void CreateAdjencyMatrix(AdiacencyListGraph g)
+        {
+            int?[,] adjanceyMatrix = new int?[g.noVertices, g.noVertices];
+
+
+            for (int parentVertex = 0; parentVertex < g.noVertices; parentVertex++)
+            {
+                var parentNode = arrLinkedList[parentVertex];
+
+
+                for (int childNode = 0; childNode < g.noVertices; childNode++)
+                {
+                    if (parentVertex != childNode)
+                    {
+                        var arc = parentNode.Find(childNode);
+
+
+                        if (arc != null)
+                        {
+                            adjanceyMatrix[parentVertex, childNode] = 1;
+                        }
+
+
+
+                    }
+                }
+
+            }
+            //PrintAdjanceyMatrix(adjanceyMatrix, graph.totalVertices);
+        }
+
+        public string PrintAdjecyMatrix(int?[,] adj, int Length)
+        {
+
+        }
 
     }
 }
